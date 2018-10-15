10ef964adaec2519e383aa08ca9129b67bd665ac
Kristian <kybanez12@gmail.com>
2018-10-15 18:28:50 +1100

    initial commit

---
+++ SimpleWPFApp/CodedUITestProject2/UIMap.cs
@@ -0,0 +1,98 @@
+﻿namespace CodedUITestProject2
+{
+    using Microsoft.VisualStudio.TestTools.UITesting.WinControls;
+    using Microsoft.VisualStudio.TestTools.UITesting.WpfControls;
+    using System;
+    using System.Collections.Generic;
+    using System.CodeDom.Compiler;
+    using Microsoft.VisualStudio.TestTools.UITest.Extension;
+    using Microsoft.VisualStudio.TestTools.UITesting;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Keyboard = Microsoft.VisualStudio.TestTools.UITesting.Keyboard;
+    using Mouse = Microsoft.VisualStudio.TestTools.UITesting.Mouse;
+    using MouseButtons = System.Windows.Forms.MouseButtons;
+    using System.Drawing;
+    using System.Windows.Input;
+    using System.Text.RegularExpressions;
+    using Microsoft.VisualStudio.TestTools.UITesting.WpfControls;
+
+
+    public partial class UIMap
+    {
+
+
+        /// <summary>
+        /// SimpleAppTest - Use 'SimpleAppTestParams' to pass parameters into this method.
+        /// </summary>
+        /// 
+
+
+        // Kristian Ybanez 101644810
+        public void ModifiedSimpleAppTest()
+        {
+            #region Variable Declarations
+            WpfCheckBox uICheckBoxCheckBox = this.UIMainWindowWindow.UICheckBoxCheckBox;
+            WpfButton uIStartButton = this.UIMainWindowWindow.UIStartButton;
+            WinButton uICloseButton = this.UIMainWindowWindow1.UICloseButton;
+            #endregion
+
+            // Launch '%USERPROFILE%\Desktop\KY\SimpleWPFApp\SimpleWPFApp\bin\Debug\SimpleWPFApp.exe'
+            ApplicationUnderTest uIMainWindowWindow = ApplicationUnderTest.Launch(this.SimpleAppTestParams.UIMainWindowWindowExePath, this.SimpleAppTestParams.UIMainWindowWindowAlternateExePath);
+
+            // Click 'CheckBox' check box
+            Mouse.Click(uICheckBoxCheckBox, new Point(65, 4));
+
+            // Click 'CheckBox' check box
+            Mouse.Click(uICheckBoxCheckBox, new Point(38, 4));
+
+            // Click 'Start' button
+            Mouse.Click(uIStartButton, new Point(63, 7));
+
+            uICheckBoxCheckBox.WaitForControlEnabled();
+
+            // Select 'CheckBox' check box
+            uICheckBoxCheckBox.Checked = this.SimpleAppTestParams.UICheckBoxCheckBoxChecked;
+
+            // Click 'Close' button
+            Mouse.Click(uICloseButton, new Point(8, 13));
+        }
+
+        public virtual SimpleAppTestParams SimpleAppTestParams
+        {
+            get
+            {
+                if ((this.mSimpleAppTestParams == null))
+                {
+                    this.mSimpleAppTestParams = new SimpleAppTestParams();
+                }
+                return this.mSimpleAppTestParams;
+            }
+        }
+
+        private SimpleAppTestParams mSimpleAppTestParams;
+    }
+    /// <summary>
+    /// Parameters to be passed into 'SimpleAppTest'
+    /// </summary>
+    [GeneratedCode("Coded UITest Builder", "15.0.26208.0")]
+    public class SimpleAppTestParams
+    {
+
+        #region Fields
+        /// <summary>
+        /// Launch '%USERPROFILE%\Desktop\KY\SimpleWPFApp\SimpleWPFApp\bin\Debug\SimpleWPFApp.exe'
+        /// </summary>
+        public string UIMainWindowWindowExePath = "C:\\Users\\STUDENT\\Desktop\\KY\\SimpleWPFApp\\SimpleWPFApp\\bin\\Debug\\SimpleWPFApp.exe";
+
+        /// <summary>
+        /// Launch '%USERPROFILE%\Desktop\KY\SimpleWPFApp\SimpleWPFApp\bin\Debug\SimpleWPFApp.exe'
+        /// </summary>
+        public string UIMainWindowWindowAlternateExePath = "%USERPROFILE%\\Desktop\\KY\\SimpleWPFApp\\SimpleWPFApp\\bin\\Debug\\SimpleWPFApp.exe";
+
+        /// <summary>
+        /// Select 'CheckBox' check box
+        /// </summary>
+        public bool UICheckBoxCheckBoxChecked = true;
+        #endregion
+    }
+}
